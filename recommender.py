c9e8e08acffd808c2a5416beee86eac449acfbcf
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2024-11-18 20:09:48 -0500

    Finish python version Create html css and js

--- recommender.py
+++ recommender.py
@@ -1,6 +1,5 @@
 import base64, requests, sys
 import spotipy
-from music_genre_classification import * 
 from spotipy.oauth2 import SpotifyClientCredentials
 
 def print_songs(l:list): 
@@ -12,7 +11,6 @@ def print_songs(l:list):
 
   for key, value in songs.items():
     print(f"{key}: {value}")
-
 
 def main():
   #Authentication - without user
@@ -30,17 +28,38 @@ def main():
   choice = int(input("Choose a song number: "))
   
   track_uri = list[choice]["uri"]
+  track_id =[list[choice]["id"]]
 
+  
   list_song_data = ["danceability", "energy", "key", 
                   "loudness", "mode", "speechiness", 
                   "acousticness", "instrumentalness", 
                   "liveness", "valence", "tempo", 
                   "time_signature"]
+
+  song_data = []
   for x in list_song_data:
-    print(sp.audio_features(track_uri)[0][x])
-    
+    #print(sp.audio_features(track_uri)[0][x])
+    song_data.append(sp.audio_features(track_uri)[0][x])
+  
+  #Get the artist spotify ID and use to search list of his/her genre(s)
+  artist_id= [list[choice]["artists"][0]["id"]]
+  artist_genres = sp.artist(artist_id[0])["genres"]
+
+  recommendations = sp.recommendations(artist_id, artist_genres,track_id, 20, "",
+                           danceability = song_data[0], energy = song_data[1],
+                           key = song_data[2], loudness = song_data[3],
+                           mode = song_data[4], speechiness = song_data[5],
+                           acousticness = song_data[6], instrulmentalness = song_data[7],
+                           liveness = song_data[8], valence = song_data[9],
+                           temp = song_data[10], time_signature = song_data[11], popularity = 25)
+  print("\n")
+  for x in recommendations["tracks"]:
+    print(x["name"] + " " + x["artists"][0]["name"])
+
+
   #MP3 preview of song
-  print(type(list[choice]["preview_url"]))
+  #print(type(list[choice]["preview_url"]))
 
 if __name__ == "__main__":
   main()
